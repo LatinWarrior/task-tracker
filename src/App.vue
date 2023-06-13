17d2f11cfdbc18251a2ca52e84b147a286dfffc4
git <luis.f.blanco@gmail.com>
2023-06-13 13:30:28 -0500

    Finish toggling reminder indicator on task.

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,11 @@
         <router-view :showAddTask="showAddTask"></router-view>        
         <Footer /> -->
         <Header title="Task Tracker" />
-        <Tasks @delete-task="deleteTask" :tasks="tasks" />
+        <Tasks
+            @toggle-reminder="toggleReminder"
+            @delete-task="deleteTask"
+            :tasks="tasks"
+        />
     </div>
 </template>
 
@@ -34,6 +38,12 @@ export default {
             if (confirm('Are you sure?')) {
                 this.tasks = this.tasks.filter((task) => task.id !== id);
             }
+        },
+        toggleReminder(id) {
+            console.log(id);
+            this.tasks = this.tasks.map((task) =>
+                task.id === id ? { ...task, reminder: !task.reminder } : task
+            );
         },
     },
     created() {
