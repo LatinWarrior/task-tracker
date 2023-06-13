8d6dea4b62b9649676f931c1172d3470a214f56f
git <luis.f.blanco@gmail.com>
2023-06-13 13:18:20 -0500

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+    <button @:click="onClick" :style="{ background: color }" class="btn">
+        {{ text }}
+    </button>
+</template>
+
+<script lang="js">
+export default {
+    // eslint-disable-next-line
+    name: 'Button',
+    props: {
+        text: String,
+        color: String
+    },
+    methods: {
+        onClick() {
+            console.log(`click`)
+        }
+    }
+};
+</script>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,21 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <Task @delete-task="$emit('delete-task', task.id)" :task="task" />
+    </div>
+</template>
+
+<script lang="js">
+import Task from './Task';
+
+export default {
+    // eslint-disable-next-line
+    name: 'Tasks',
+    props: {
+        tasks: Array,
+    },
+    components: {
+        Task,
+    },
+    emits: ['delete-task']
+};
+</script>
